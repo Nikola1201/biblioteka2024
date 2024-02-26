6121d603665abc327beca6b655b1f6a94663924c
Nikola1201 <nikola.icic12@gmail.com>
2024-02-26 14:39:16 +0100

    Uvedena klasa autor, promenjena Knjiga sa listom Autora

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -1,11 +1,17 @@
 package biblioteka;
 
+import java.util.List;
+
 public class Knjiga {
 	
 	private String naslov;
+	
 	private long isbn;
-	private String autori;
+	
+	private List<Autor> autori;
+	
 	private String izdavac;
+	
 	private int izdanje;
 	
 	public String getNaslov() {
@@ -20,10 +26,11 @@ public class Knjiga {
 	public void setIsbn(long isbn) {
 		this.isbn = isbn;
 	}
-	public String getAutori() {
+	
+	public List<Autor> getAutori() {
 		return autori;
 	}
-	public void setAutori(String autori) {
+	public void setAutori(List<Autor> autori) {
 		this.autori = autori;
 	}
 	public String getIzdavac() {
