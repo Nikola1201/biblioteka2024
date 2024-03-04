1cfc962320c26a89a28fd04d4995c42b0f5aee68
Nikola1201 <nikola.icic12@gmail.com>
2024-03-04 12:56:56 +0100

    Dodata logicka kontrola u klasu Biblioteka

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -1,33 +1,50 @@
 package biblioteka;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import biblioteka.interfejs.BibliotekaInterfejs;
 
 public class Biblioteka implements BibliotekaInterfejs {
 
+	private List<Knjiga> knjige = new ArrayList<Knjiga>();
+
 	@Override
 	public void dodajKnjigu(Knjiga k) {
-		// TODO Auto-generated method stub
+		if (k == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		if (knjige.contains(k))
+			throw new IllegalArgumentException("Knjiga vec postoji u biblioteci");
 
+		knjige.add(k);
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga k) {
-		// TODO Auto-generated method stub
-
+		if (k == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		if (!knjige.contains(k))
+			throw new IllegalArgumentException("Knjiga ne postoji u biblioteci");
+		knjige.remove(k);
 	}
 
 	@Override
 	public List<Knjiga> vratiSveKnjige() {
-		// TODO Auto-generated method stub
-		return null;
+		return knjige;
 	}
 
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
-		// TODO Auto-generated method stub
-		return null;
+		if (autor == null && isbn == 0 && naslov == null && izdavac == null)
+			throw new IllegalArgumentException("Morate imati barem jedan kriterijum");
+
+		List<Knjiga> result = new ArrayList<Knjiga>();
+
+		for (Knjiga knjiga : knjige) {
+			if (knjiga.getNaslov().toUpperCase().contains(naslov.toUpperCase()))
+				result.add(knjiga);
+		}
+		return result;
 	}
 
 }
