2c50320c8395a2a46ddec04a1e860f586dc3854d
Nikola1201 <nikola.icic12@gmail.com>
2024-03-11 12:50:59 +0100

    Dodata logicka kontrola za klase Autor i Knjiga

--- Biblioteka/src/biblioteka/Autor.java
+++ Biblioteka/src/biblioteka/Autor.java
@@ -7,12 +7,24 @@ public class Autor {
 	private String ime;
 
 	private String prezime;
+	
+	public Autor() {}
+	
+	public Autor(String ime,String prezime) {
+		this.setIme(ime);
+		this.setPrezime(prezime);
+	}
 
 	public String getIme() {
 		return ime;
 	}
 
 	public void setIme(String ime) {
+		if(ime==null)
+			throw new NullPointerException("Ime ne sme biti null");
+		if(ime.isEmpty())
+			throw new IllegalArgumentException("Ime ne sme biti prazno");
+		
 		this.ime = ime;
 	}
 
@@ -21,6 +33,10 @@ public class Autor {
 	}
 
 	public void setPrezime(String prezime) {
+		if(prezime==null)
+			throw new NullPointerException("Prezime ne sme biti null");
+		if(prezime.isEmpty())
+			throw new IllegalArgumentException("Prezime ne sme biti prazno");
 		this.prezime = prezime;
 	}
 
@@ -45,5 +61,5 @@ public class Autor {
 		Autor other = (Autor) obj;
 		return Objects.equals(ime, other.ime) && Objects.equals(prezime, other.prezime);
 	}
-
+	
 }
--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -21,6 +21,10 @@ public class Knjiga {
 	}
 
 	public void setNaslov(String naslov) {
+		if(naslov==null)
+			throw new NullPointerException("Naslov ne sme biti null");
+		if(naslov.isEmpty())
+			throw new IllegalArgumentException("Naslov ne sme biti prazno");
 		this.naslov = naslov;
 	}
 
@@ -29,6 +33,8 @@ public class Knjiga {
 	}
 
 	public void setIsbn(long isbn) {
+		if(isbn<1000000000 || isbn > 9999999999999L)
+			throw new IllegalArgumentException("ISBN mora imate bar 10 cifara a manje od 13");
 		this.isbn = isbn;
 	}
 
@@ -53,6 +59,8 @@ public class Knjiga {
 	}
 
 	public void setIzdanje(int izdanje) {
+		if(izdanje<1)
+			throw new IllegalArgumentException("Izdanje mora biti 1 ili vece");
 		this.izdanje = izdanje;
 	}
 
